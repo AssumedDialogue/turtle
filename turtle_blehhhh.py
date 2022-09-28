8b6d2c8f6dcfc86eb71edb2d36df974050e35c24
AssumedDialogue <114254777+AssumedDialogue@users.noreply.github.com>
2022-09-28 16:56:47 -0400

    any message

--- turtle_blehhhh.py
+++ turtle_blehhhh.py
@@ -4,3 +4,7 @@ alex = turtle.Turtle()    # create a turtle named alex
 alex.forward(150)         # tell alex to move forward by 150 units
 alex.left(90)             # turn by 90 degrees
 alex.forward(75)          # complete the second side of a rectangle
+alex.left(90) 
+alex.forward(150)
+alex.left(90)
+alex.forward(75)
